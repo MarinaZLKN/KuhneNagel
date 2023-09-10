236fcb3eab46f4356adafec74df0a67d4ca59de6
Marina <marina.zilkina@gmail.com>
2023-09-11 00:38:20 +0300

    Tests for api added

--- actions.test.js
+++ actions.test.js
@@ -1,61 +1,225 @@
 import {
   DELETE_SHIPMENT,
-  deleteShipment,
-  SHOW_SHIPMENTS,
-  showShipments,
+  deleteShipment, fetchShipments,
+  SHOW_SHIPMENTS, showShipments,
   UPDATE_SHIPMENT,
   updateShipment
 } from "./src/actions/actions";
+import mockAxios from "jest-mock-axios";
 
+//Tests for the API requests
 
-describe('showShipments action creator', () => {
-  it('should return an action to show shipments', () => {
-    const mockShipments = [
-      {
-        orderNo: '123',
-        date: '2021-01-01',
-        customer: 'Customer1',
-        trackingNo: 'tracking123',
-        status: 'In Transit',
-        consignee: 'Consignee1'
-      },
-    ];
+describe('fetchShipments action', () => {
+  afterEach(() => {
+    mockAxios.reset();
+    jest.clearAllMocks();
+  });
 
-    const expectedAction = {
-      type: SHOW_SHIPMENTS,
-      payload: mockShipments,
-    };
+  describe('when API call is successful', () => {
+    it('should dispatch SHOW_SHIPMENTS', async () => {
+      const mockData = [
+        { orderNo: "kk-275651-64476049-3346442",
+          date: "8/20/2019",
+          customer: "Triumph Bancorp, Inc.",
+          trackingNo: "TP-011637-13598236-2700556",
+          status: "'Delivered'",
+          consignee: "Celsius Holdings, Inc."
+        },
+        { orderNo: "kl-235651-64476039-3346442",
+          date: "12/20/2014",
+          customer: "Triumph Bancorp, Inc.",
+          trackingNo: "TP-022637-13598236-2700556",
+          status: "'Not delivered'",
+          consignee: "Celsius Holdings, Inc."},
+        ];
 
-    expect(showShipments(mockShipments)).toEqual(expectedAction);
+      mockAxios.get.mockResolvedValueOnce({data: mockData});
+
+      const dispatch = jest.fn();
+
+      await fetchShipments()(dispatch);
+
+      expect(mockAxios.get).toHaveBeenCalledWith('https://my.api.mockaroo.com/shipments.json?key=5e0b62d0');
+      expect(dispatch).toHaveBeenCalledWith({ type: SHOW_SHIPMENTS, payload: mockData });
+    });
+  });
+
+  describe('when API call fails', () => {
+    it('should log an error', async () => {
+      const errorMessage = "Network Error";
+
+      const consoleErrorSpy = jest.spyOn(console, 'error');
+
+      mockAxios.get.mockRejectedValueOnce(new Error(errorMessage));
+
+      const dispatch = jest.fn();
+
+      await fetchShipments()(dispatch);
+
+      expect(mockAxios.get).toHaveBeenCalledWith('https://my.api.mockaroo.com/shipments.json?key=5e0b62d0');
+      expect(consoleErrorSpy).toHaveBeenCalled();
+    });
+  });
+});
+
+jest.spyOn(console, 'error').mockImplementation(() => {});
+
+describe('deleteShipment action', () => {
+  afterEach(() => {
+    mockAxios.reset();
+    jest.clearAllMocks();
+  });
+
+  describe('when API call is successful', () => {
+    it('should dispatch DELETE_SHIPMENT', async () => {
+      const orderNo = 'kk-275651-64476049-3346442';
+      const mockData = { message: 'Shipment deleted successfully' };
+
+      mockAxios.delete.mockResolvedValueOnce({ data: mockData });
+
+      const dispatch = jest.fn();
+
+      await deleteShipment(orderNo)(dispatch);
+
+      expect(mockAxios.delete).toHaveBeenCalledWith(`https://my.api.mockaroo.com/shipments.json/${orderNo}?key=5e0b62d0`);
+      expect(dispatch).toHaveBeenCalledWith({ type: DELETE_SHIPMENT, payload: mockData });
+    });
+  });
+
+  describe('when API call fails', () => {
+    it('should log an error', async () => {
+      const orderNo = 'kk-275651-64476049-3346442';
+      const errorMessage = "Network Error";
+
+      const consoleErrorSpy = jest.spyOn(console, 'error');
+
+      mockAxios.delete.mockRejectedValueOnce(new Error(errorMessage));
+
+      const dispatch = jest.fn();
+
+      await deleteShipment(orderNo)(dispatch);
+
+      expect(mockAxios.delete).toHaveBeenCalledWith(`https://my.api.mockaroo.com/shipments.json/${orderNo}?key=5e0b62d0`);
+      expect(consoleErrorSpy).toHaveBeenCalled();
+    });
   });
 });
 
 
-describe('deleteShipment action creator', () => {
-  it('should return an action to delete a shipment', () => {
-    const mockOrderNo = '123';
+describe("updateShipment action", () => {
+  afterEach(() => {
+    mockAxios.reset();
+    jest.clearAllMocks();
+  });
 
-    const expectedAction = {
-      type: DELETE_SHIPMENT,
-      payload: mockOrderNo,
-    };
+  describe("when API call is successful", () => {
+    it("should dispatch UPDATE_SHIPMENT", async () => {
+      const updatedShipment = {
+        orderNo: "kk-275651-64476049-3346442",
+        date: "8/20/2019",
+        customer: "Triumph Bancorp, Inc.",
+        trackingNo: "TP-011637-13598236-2700556",
+        status: "'Delivered'",
+        consignee: "Celsius Holdings, Inc."
 
-    expect(deleteShipment(mockOrderNo)).toEqual(expectedAction);
+      };
+      const mockData = {
+        message: "Shipment updated successfully",
+      };
+
+      mockAxios.put.mockResolvedValueOnce({ data: mockData });
+
+      const dispatch = jest.fn();
+
+      await updateShipment(updatedShipment)(dispatch);
+
+      expect(mockAxios.put).toHaveBeenCalledWith(
+        `https://my.api.mockaroo.com/shipments.json/${updatedShipment.orderNo}?key=5e0b62d0`,
+        updatedShipment
+      );
+      expect(dispatch).toHaveBeenCalledWith({ type: UPDATE_SHIPMENT, payload: mockData });
+    });
+  });
+
+  describe("when API call fails", () => {
+    it("should log an error", async () => {
+      const updatedShipment = {
+        orderNo: "kk-275651-64476049-3346442",
+        date: "8/20/2019",
+        customer: "Triumph Bancorp, Inc.",
+        trackingNo: "TP-011637-13598236-2700556",
+        status: "'Delivered'",
+        consignee: "Celsius Holdings, Inc."
+      };
+      const errorMessage = "Network Error";
+
+      const consoleErrorSpy = jest.spyOn(console, "error");
+
+      mockAxios.put.mockRejectedValueOnce(new Error(errorMessage));
+
+      const dispatch = jest.fn();
+
+      await updateShipment(updatedShipment)(dispatch);
+
+      expect(mockAxios.put).toHaveBeenCalledWith(
+        `https://my.api.mockaroo.com/shipments.json/${updatedShipment.orderNo}?key=5e0b62d0`,
+        updatedShipment
+      );
+      expect(consoleErrorSpy).toHaveBeenCalled();
+    });
   });
 });
 
-describe('updateShipment action creator', () => {
-  it('should return an action to update a shipment', () => {
-    const mockUpdatedShipment = {
-      orderNo: '123',
-      name: 'Updated Shipment',
-    };
+//Tests for the .json file actions
 
-    const expectedAction = {
-      type: UPDATE_SHIPMENT,
-      payload: mockUpdatedShipment,
-    };
-
-    expect(updateShipment(mockUpdatedShipment)).toEqual(expectedAction);
-  });
-});
+// describe('showShipments action creator', () => {
+//   it('should return an action to show shipments', () => {
+//     const mockShipments = [
+//       {
+//         orderNo: '123',
+//         date: '2021-01-01',
+//         customer: 'Customer1',
+//         trackingNo: 'tracking123',
+//         status: 'In Transit',
+//         consignee: 'Consignee1'
+//       },
+//     ];
+//
+//     const expectedAction = {
+//       type: SHOW_SHIPMENTS,
+//       payload: mockShipments,
+//     };
+//
+//     expect(showShipments(mockShipments)).toEqual(expectedAction);
+//   });
+// });
+//
+//
+// describe('deleteShipment action creator', () => {
+//   it('should return an action to delete a shipment', () => {
+//     const mockOrderNo = 'kk-275651-64476049-3346442';
+//
+//     const expectedAction = {
+//       type: DELETE_SHIPMENT,
+//       payload: mockOrderNo,
+//     };
+//
+//     expect(deleteShipment(mockOrderNo)).toEqual(expectedAction);
+//   });
+// });
+//
+// describe('updateShipment action creator', () => {
+//   it('should return an action to update a shipment', () => {
+//     const mockUpdatedShipment = {
+//       orderNo: 'kk-275651-64476049-3346442',
+//       name: 'Updated Shipment',
+//     };
+//
+//     const expectedAction = {
+//       type: UPDATE_SHIPMENT,
+//       payload: mockUpdatedShipment,
+//     };
+//
+//     expect(updateShipment(mockUpdatedShipment)).toEqual(expectedAction);
+//   });
+// });
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -4,35 +4,66 @@ export const SHOW_SHIPMENTS = 'SHOW_SHIPMENTS';
 export const DELETE_SHIPMENT = 'DELETE_SHIPMENT';
 export const UPDATE_SHIPMENT = 'UPDATE_SHIPMENT';
 
-export const showShipments = (shipments) => ({
-  type: SHOW_SHIPMENTS,
-  payload: shipments,
-});
+//This part is responsible for API requests, but since I have only one endpoint for displaying the data on a visual part update and delete logic won't work
+//below is the logic for working with .json file
+export const updateShipment = (updatedShipment) => async (dispatch) => {
+  try {
+    const response = await axios.put(`https://my.api.mockaroo.com/shipments.json/${updatedShipment.orderNo}?key=5e0b62d0`, updatedShipment);
+    dispatch({
+      type: UPDATE_SHIPMENT,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.error("Error updating shipment:", error);
+  }
+};
 
-
-export const deleteShipment = (orderNo) => ({
-  type: DELETE_SHIPMENT,
-  payload: orderNo,
-});
-
-
-export const updateShipment = (updatedShipment) => ({
-  type: UPDATE_SHIPMENT,
-  payload: updatedShipment,
-});
-
-
+export const deleteShipment = (orderNo) => async (dispatch) => {
+  try {
+    const url = `https://my.api.mockaroo.com/shipments.json/${orderNo}?key=5e0b62d0`;
+    const response = await axios.delete(url);
+    dispatch({
+      type: DELETE_SHIPMENT,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.error("Error deleting shipment:", error);
+  }
+};
 
 export const fetchShipments = () => async (dispatch) => {
   try {
     const response = await axios.get('https://my.api.mockaroo.com/shipments.json?key=5e0b62d0');
     const data = response.data;
-    dispatch(showShipments(data));
+    dispatch({
+      type: SHOW_SHIPMENTS,
+      payload: data
+    });
   } catch (error) {
     console.error("Error in actions/endpoint:", error);
   }
 };
 
+//This logic is responsible for working with .json file - you will see the result on visual part
+
+
+//
+// export const showShipments = (shipments) => ({
+//   type: SHOW_SHIPMENTS,
+//   payload: shipments,
+// });
+//
+//
+// export const updateShipment = (updatedShipment) => ({
+//   type: UPDATE_SHIPMENT,
+//   payload: updatedShipment,
+// });
+//
+// export const deleteShipment = (orderNo) => ({
+//   type: DELETE_SHIPMENT,
+//   payload: orderNo,
+// });
+//
 // export const fetchShipments = () => async (dispatch) => {
 //   try {
 //     const response = await fetch('./shipments1.json');
@@ -42,33 +73,4 @@ export const fetchShipments = () => async (dispatch) => {
 //     console.error("Error in actions/json-file:", error);
 //   }
 // };
-
-//TODO i need an API docs for put and delete requests
-
-// export const deleteShipment = (orderNo) => async (dispatch) => {
-//   try {
-//     const url = `https://my.api.mockaroo.com/shipments.json/${orderNo}?key=5e0b62d0`;
-//     const response = await axios.delete(url);
-//     dispatch({
-//       type: 'DELETE_SHIPMENT',
-//       payload: response.data,
-//     });
-//   } catch (error) {
-//     console.error("Error deleting shipment:", error);
-//     console.error("Error details:", error.response);
-//   }
-// };
 //
-//
-//
-// export const updateShipment = (updatedShipment) => async (dispatch) => {
-//   try {
-//     const response = await axios.put(`https://my.api.mockaroo.com/shipments.json/${updatedShipment.orderNo}?key=5e0b62d0`, updatedShipment);
-//     dispatch({
-//       type: UPDATE_SHIPMENT,
-//       payload: response.data,
-//     });
-//   } catch (error) {
-//     console.error("Error updating shipment:", error);
-//   }
-// };
