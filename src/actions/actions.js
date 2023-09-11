462f3cfe23252409e8e583a08423cd69d90389ea
Marina <marina.zilkina@gmail.com>
2023-09-11 14:28:33 +0300

    ready

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -4,33 +4,7 @@ export const SHOW_SHIPMENTS = 'SHOW_SHIPMENTS';
 export const DELETE_SHIPMENT = 'DELETE_SHIPMENT';
 export const UPDATE_SHIPMENT = 'UPDATE_SHIPMENT';
 
-//This part is responsible for API requests, but since I have only one endpoint for displaying the data on a visual part update and delete logic won't work
-//below is the logic for working with .json file
-export const updateShipment = (updatedShipment) => async (dispatch) => {
-  try {
-    const response = await axios.put(`https://my.api.mockaroo.com/shipments.json/${updatedShipment.orderNo}?key=5e0b62d0`, updatedShipment);
-    dispatch({
-      type: UPDATE_SHIPMENT,
-      payload: response.data,
-    });
-  } catch (error) {
-    console.error("Error updating shipment:", error);
-  }
-};
-
-export const deleteShipment = (orderNo) => async (dispatch) => {
-  try {
-    const url = `https://my.api.mockaroo.com/shipments.json/${orderNo}?key=5e0b62d0`;
-    const response = await axios.delete(url);
-    dispatch({
-      type: DELETE_SHIPMENT,
-      payload: response.data,
-    });
-  } catch (error) {
-    console.error("Error deleting shipment:", error);
-  }
-};
-
+// If endpoint is available use first 3 actions
 export const fetchShipments = () => async (dispatch) => {
   try {
     const response = await axios.get('https://my.api.mockaroo.com/shipments.json?key=5e0b62d0');
@@ -44,26 +18,23 @@ export const fetchShipments = () => async (dispatch) => {
   }
 };
 
-//This logic is responsible for working with .json file - you will see the result on visual part
+export const updateShipment = (updatedShipment) => ({
+  type: UPDATE_SHIPMENT,
+  payload: updatedShipment,
+});
 
+export const deleteShipment = (orderNo) => ({
+  type: DELETE_SHIPMENT,
+  payload: orderNo,
+});
 
-//
+//If endpoint is not available, lets work with .json file (comment the fetchShipments function with GET request)
+
 // export const showShipments = (shipments) => ({
 //   type: SHOW_SHIPMENTS,
 //   payload: shipments,
 // });
-//
-//
-// export const updateShipment = (updatedShipment) => ({
-//   type: UPDATE_SHIPMENT,
-//   payload: updatedShipment,
-// });
-//
-// export const deleteShipment = (orderNo) => ({
-//   type: DELETE_SHIPMENT,
-//   payload: orderNo,
-// });
-//
+
 // export const fetchShipments = () => async (dispatch) => {
 //   try {
 //     const response = await fetch('./shipments1.json');
@@ -73,4 +44,34 @@ export const fetchShipments = () => async (dispatch) => {
 //     console.error("Error in actions/json-file:", error);
 //   }
 // };
+
+
+
+// This part is responsible for API requests, but since I have only one endpoint for displaying the data on a visual part update and delete logic won't work
+// if you want to check this, do not forget to uncomment tests for this and comment actions above
+
+// export const updateShipment = (updatedShipment) => async (dispatch) => {
+//   try {
+//     const response = await axios.put(`https://my.api.mockaroo.com/shipments.json/${updatedShipment.orderNo}?key=5e0b62d0`, updatedShipment);
+//     dispatch({
+//       type: UPDATE_SHIPMENT,
+//       payload: response.data,
+//     });
+//   } catch (error) {
+//     console.error("Error updating shipment:", error);
+//   }
+// };
 //
+// export const deleteShipment = (orderNo) => async (dispatch) => {
+//   try {
+//     const url = `https://my.api.mockaroo.com/shipments.json/${orderNo}?key=5e0b62d0`;
+//     const response = await axios.delete(url);
+//     dispatch({
+//       type: DELETE_SHIPMENT,
+//       payload: response.data,
+//     });
+//   } catch (error) {
+//     console.error("Error deleting shipment:", error);
+//   }
+// };
+
